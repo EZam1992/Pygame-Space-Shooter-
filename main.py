e56d5369c35336496d39930ec76b2241054bbda5
Erick Zamora <erickzamora@Mac.lan>
2025-03-29 11:52:49 -0700

    added meteor and laser

--- main.py
+++ main.py
@@ -9,10 +9,15 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Space Shooter")
 running = True 
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
-
-#importing an image 
+player_direction = 1
+#imports
 player_surf = pygame.image.load(join('space shooter', 'images', 'player.png')).convert_alpha()
+player_rect = player_surf.get_frect(center = (WINDOW_WIDTH/2,WINDOW_HEIGHT/2))
 star_surf = pygame.image.load(join('space shooter', 'images', 'star.png')).convert_alpha()
+meteor_surf = pygame.image.load(join('space shooter', 'images', 'meteor.png')).convert_alpha()
+meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH/2,WINDOW_HEIGHT/2))
+laser_surf = pygame.image.load(join('space shooter', 'images', 'laser.png')).convert_alpha()
+laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
 
 while running:
     # event loop 
@@ -24,9 +29,17 @@ while running:
 
     #draw the game 
     display_surface.fill("darkgray")
-    display_surface.blit(player_surf, (100, 200))
     for pos in star_positions:
         display_surface.blit(star_surf, pos)
+        
+    display_surface.blit(meteor_surf, meteor_rect)
+    display_surface.blit(laser_surf, laser_rect)
+
+    #player movement 
+    player_rect.x += player_direction * 0.4
+    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
+        player_direction *= -1
+    display_surface.blit(player_surf, player_rect)
     pygame.display.update()
 
 pygame.quit()
