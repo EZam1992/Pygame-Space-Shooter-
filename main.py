798f9078642af759e071e3dcdd5438b45e145567
Erick Zamora <erickzamora@Mac.lan>
2025-04-03 16:32:21 -0700

    refined player movement  #Please enter the commit message for your changes. Lines starting

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Space Shooter")
 running = True 
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
-player_direction = 1
+player_direction = pygame.math.Vector2(1, 1)
+player_speed = 300
+clock = pygame.time.Clock()
+
 #imports
 player_surf = pygame.image.load(join('space shooter', 'images', 'player.png')).convert_alpha()
 player_rect = player_surf.get_frect(center = (WINDOW_WIDTH/2,WINDOW_HEIGHT/2))
@@ -20,12 +23,11 @@ laser_surf = pygame.image.load(join('space shooter', 'images', 'laser.png')).con
 laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
 
 while running:
+    dt = clock.tick() / 1000
     # event loop 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False 
-
-
 
     #draw the game 
     display_surface.fill("darkgray")
@@ -36,9 +38,13 @@ while running:
     display_surface.blit(laser_surf, laser_rect)
 
     #player movement 
-    player_rect.x += player_direction * 0.4
+    player_rect.center += player_direction * player_speed * dt
+    if player_rect.bottom > WINDOW_HEIGHT or player_rect.top < 0:
+        player_direction.y *= -1
+
     if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
-        player_direction *= -1
+        player_direction.x *= -1
+
     display_surface.blit(player_surf, player_rect)
     pygame.display.update()
 
